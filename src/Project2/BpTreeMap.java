753f40d4efab850cf103db7cd8e5bb1a9afe8f27
Benjamin Kovach] <bkovach5@uga.edu>
2014-10-07 17:18:59 -0400

    Add getters for L/R pointers of a key

--- src/Project2/BpTreeMap.java
+++ src/Project2/BpTreeMap.java
@@ -76,7 +76,28 @@ public class BpTreeMap <K extends Comparable <K>, V>
             for(int i = 0; i < refList.size(); i++) {
                 this.ref[i] = refList.get(i);
             }
+
+            this.nKeys--;
         }
+
+        // Get the left pointer from a key
+        public Object leftPointer(K k) {
+            int idx = Arrays.binarySearch(key, k);
+            if(idx == -1) {
+                return null;
+            }
+            return ref[idx];
+        }
+
+        // Get the right pointer from a key
+        public Object rightPointer(K k) {
+            int idx = Arrays.binarySearch(key, k);
+            if(idx == -1) {
+                return null;
+            }
+            return ref[idx + 1];
+        }
+
     } // Node inner class
 
     /** The root of the B+Tree
@@ -310,6 +331,21 @@ public class BpTreeMap <K extends Comparable <K>, V>
         return (n.isLeaf) ? null : find (key, (Node) n.ref [n.nKeys]);
     } // find
 
+
+    // Insert a value into the root of the tree
+    private void insertRoot(K key, V ref, Node n) {
+
+    }
+
+    // Insert a k/v pair into an inner node 
+    private void insertInner(K key, Node ref, Node n) {
+
+    }
+
+    private Map.Entry<K, Node> insertLeaf(K key, V ref, Node n) {
+        return null;
+    }
+
     /********************************************************************************
      * Recursive helper function for inserting a key in B+trees.
      * @param key  the key to insert
@@ -317,52 +353,113 @@ public class BpTreeMap <K extends Comparable <K>, V>
      * @param n    the current node
      * @param p    the parent node
      */
-    // @TODO: Fix type errors with V/Object for refs
-    private Map.Entry<K,V> insert (K key, V ref, Node n, Node p)
+    private Map.Entry<K,Node> insert (K key, V ref, Node n, Node p)
     {
-        for(int i = 0; i < n.nKeys; i++) {
-            K curKey = n.key[i];
-            if(key.compareTo(curKey) <= 0) {
-                if(n.isLeaf) {
-                    // proper key to insert into
-                    if(n.nKeys >= ORDER - 1) {
-                        // Must split!
-                        Node splitNode = split(key, ref, n);
-                        // Need to insert min value from split leaf into parent.
-                        return new AbstractMap.SimpleEntry<K,V>(splitNode.key[0], splitNode.ref[0]);
-                    } else {
-                        // there is space; wedge key/val pair in
-                        wedgeOrdered(key, ref, n);
-                        return null;
-                    }
+        out.println ("Insert");
+        
+        if(n == root) {
+            if(n.nKeys < ORDER - 1) {
+                wedgeOrdered(key, ref, root);
+                return null;
+            }
+        }
+
+        if(n.isLeaf) {
+            // If node is leaf, then ref is a value.
+            // proper key to insert into
+            if(n.nKeys >= ORDER - 1) {
+                // Must split!
+                out.println(key);
+                out.println(ref);
+                out.println(n);
+                Node splitNode = split(key, ref, n);
+                // Need to insert min value from split leaf into parent.
+                // Reference set to this node
+                return new AbstractMap.SimpleEntry<K,Node>(splitNode.key[0], n);
+            } else {
+                // there is space; wedge key/val pair in
+                wedgeOrdered(key, ref, n);
+                return null;
+            }
+        } else {
+            // inner node
+            Node nextRef = null;
+
+            // Get the reference to follow
+            for(int i = 0; i < n.nKeys; i++) {
+                if(key.compareTo(n.key[i]) < 0 ) {
+                    nextRef = (Node) n.ref[i];
+                    break;
+                }
+            }
+
+            if(nextRef == null) {
+                // Take farthest right reference
+                nextRef = (Node) n.ref[n.nKeys];
+            }
+
+            // Insert into root and check if it overflows
+            Map.Entry<K,Node> overflow = insert(key, ref, (Node) nextRef, n);
+            
+            // If we have something to insert, insert it (but we're not a leaf!)
+            if (overflow != null) {
+                // Need to propagate overflow!
+                if(n.nKeys >= ORDER - 1) {
+                    Node splitNode = split(overflow.getKey(), ref, n);
+                    K newKey = splitNode.key[0];
+                    // Remove first element of split node
+                    splitNode.removeFirst();
+                    // Return overflow to be processed above
+                    return new AbstractMap.SimpleEntry<K,Node>(newKey, n);
                 } else {
-                    // Insert into root and check if it overflows
-                    Map.Entry<K,V> overflow = insert(key, ref, n.ref[i], n);
-                    
-                    // If we have something to insert, insert it (but we're not a leaf!)
-                    if (overflow != null) {
-                        // Need to propagate overflow!
-                        if(n.nKeys >= ORDER - 1) {
-                            Node splitNode = split(overflow.getKey(), overflow.getValue(), n);
-                            K newKey = splitNode.key[0]; V newRef = splitNode.ref[0];
-                            // Remove first element of split node
-                            splitNode.removeFirst();
-                            // Return overflow
-                            return new AbstractMap.SimpleEntry<K,V>(newKey, newRef);
-                        } else {
-                            // If there is space, just wedge the key/value pair in (no overflow)
-                            wedgeOrdered(key, ref, n);
-                            return null;
-                        }
-                    }
+                    // If there is space, just wedge the key/value pair in (no overflow)
+                    wedgeOrdered(key, ref, n);
+                    return null;
                 }
             }
         }
 
-        if(n.equals(this.root)) {
-            // wedgeOrdered(key, ref, n, i);
-        }
-        // Map.Entry<K, V> entry = new AbstractMap.SimpleEntry<K, V>(key, ref);
+        // for(int i = 0; i < n.nKeys; i++) {
+        //     K curKey = n.key[i];
+        //     if(key.compareTo(curKey) <= 0) {
+        //         if(n.isLeaf) {
+        //             // If node is leaf, then ref is a value.
+        //             // proper key to insert into
+        //             if(n.nKeys >= ORDER - 1) {
+        //                 // Must split!
+        //                 Node splitNode = split(key, ref, n);
+        //                 // Need to insert min value from split leaf into parent.
+        //                 // Reference set to this node
+        //                 return new AbstractMap.SimpleEntry<K,Node>(splitNode.key[0], n);
+        //             } else {
+        //                 // there is space; wedge key/val pair in
+        //                 wedgeOrdered(key, ref, n);
+        //                 return null;
+        //             }
+        //         } else {
+        //             // Insert into root and check if it overflows
+        //             Map.Entry<K,Node> overflow = insert(key, ref, (Node) n.ref[i], n);
+                    
+        //             // If we have something to insert, insert it (but we're not a leaf!)
+        //             if (overflow != null) {
+        //                 // Need to propagate overflow!
+        //                 if(n.nKeys >= ORDER - 1) {
+        //                     Node splitNode = split(overflow.getKey(), ref, n);
+        //                     K newKey = splitNode.key[0];
+        //                     // Remove first element of split node
+        //                     splitNode.removeFirst();
+        //                     // Return overflow to be processed above
+        //                     return new AbstractMap.SimpleEntry<K,Node>(newKey, n);
+        //                 } else {
+        //                     // If there is space, just wedge the key/value pair in (no overflow)
+        //                     wedgeOrdered(key, ref, n);
+        //                     return null;
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
+        return null;
     }
 
     /********************************************************************************
@@ -389,7 +486,7 @@ public class BpTreeMap <K extends Comparable <K>, V>
     private void wedgeOrdered(K key, V ref, Node n) {
         Node current = n;
         int i = 0;
-        while(current.key[i].compareTo(key) < 0) {
+        while(n.nKeys != 0 && i < n.nKeys && current.key[i] != null && current.key[i].compareTo(key) < 0) {
             i++;
         }
         wedge(key, ref, n, i);
@@ -480,23 +577,25 @@ public class BpTreeMap <K extends Comparable <K>, V>
     public static void main (String [] args)
     {
         BpTreeMap <Integer, Integer> bpt = new BpTreeMap <> (Integer.class, Integer.class);
-        int totKeys = 4;
-        if (args.length == 1) totKeys = Integer.valueOf (args [0]);
+        int totKeys = 5;
+        // if (args.length == 1) totKeys = Integer.valueOf (args [0]);
         for (int i = 0; i <= totKeys; i += 1) bpt.put (i, i * i);
-        bpt.print (bpt.root, 0);
-        for (int i = 0; i < totKeys; i++) {
-            out.println ("key = " + i + " value = " + bpt.get (i));
-        } // for
-        out.println ("-------------------------------------------");
-        out.println ("Average number of nodes accessed = " + bpt.count / (double) totKeys);
-        SortedMap sm = bpt.headMap(9);
-        SortedMap sm2 = bpt.tailMap(1);
-        SortedMap sm3 = bpt.subMap(1, 4);
-        out.println (sm.toString());
-        out.println (sm2.toString());
-        out.println (sm3.toString());
-        Set <Entry<Integer, Integer>> es = bpt.entrySet();
-        out.println(es.toString());
+        bpt.print(bpt.root, 0);
+
+        // bpt.print (bpt.root, 0);
+        // for (int i = 0; i < totKeys; i++) {
+        //     out.println ("key = " + i + " value = " + bpt.get (i));
+        // } // for
+        // out.println ("-------------------------------------------");
+        // out.println ("Average number of nodes accessed = " + bpt.count / (double) totKeys);
+        // SortedMap sm = bpt.headMap(9);
+        // SortedMap sm2 = bpt.tailMap(1);
+        // SortedMap sm3 = bpt.subMap(1, 4);
+        // out.println (sm.toString());
+        // out.println (sm2.toString());
+        // out.println (sm3.toString());
+        // Set <Entry<Integer, Integer>> es = bpt.entrySet();
+        // out.println(es.toString());
     } // main
 
 } // BpTreeMap class
