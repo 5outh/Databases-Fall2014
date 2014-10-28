9dc67e4df3fdb89edf06805ded0c6e7ca1f86166
Benjamin Kovach] <bkovach5@uga.edu>
2014-10-28 10:57:51 -0400

    Add getFlightStatusNow

--- src/TermProject/flight_tracker.py
+++ src/TermProject/flight_tracker.py
@@ -1,12 +1,37 @@
 import requests
+from datetime import *
 import json
 
-appID = "8efacea7"
+class FlightTracker:
+    def __init__(self, appId, appKey):
+        self.appId = appId
+        self.appKey = appKey
 
+    def flightStatus(self, carrier, flight, year, month, day):
+        url = 'https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/status/' 
+        url += carrier
+        url += '/' + flight
+        url += '/dep'
+        url += '/' + str(year)
+        url += '/' + str(month)
+        url += '/' + str(day)
+        url += ('?appId=' + self.appId + '&appKey=' + self.appKey + '&utc=false')
+        return url
+
+    def getFlightStatus(self, carrier, flight, year, month, day):
+        url = self.flightStatus(carrier, flight, year, month, day)
+        res = requests.get(url)
+        return res.json()
+
+    def getFlightStatusNow(self, carrier, flight):
+        now = datetime.now()
+        return self.getFlightStatus(carrier, flight, now.year, now.month, now.day)
+
+appId = "8efacea7"
 appKey = "ff4a4e0b9806af76726ca8ab5122cded"
 
-url = "https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/status/AA/100/dep/2014/10/27?appId=" + appID + "&appKey=" + appKey + "&utc=false"
+flightTracker = FlightTracker(appId, appKey)
 
-res = requests.get(url).json()
+flightStatus = FlightTracker(appId, appKey).getFlightStatusNow('AA', '100')
 
-print(json.dumps(res, indent=2))
+print (json.dumps(flightStatus, indent=2))
