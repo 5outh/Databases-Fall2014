19eb0df3516e02513e16b9b8374e572a10da1ca9
Benjamin Kovach] <bkovach5@uga.edu>
2014-12-01 20:58:35 -0500

    Parse dates

--- src/TermProject/flight_calculator.py
+++ src/TermProject/flight_calculator.py
@@ -1,4 +1,27 @@
 import cymysql
+from dateutil.parser import parse
+from datetime import datetime
+
+## GLOBALS
+FLIGHT_SPEED = 529 # average mph of a commercial flight
+HOURLY_PAY = 58.73 # hourly pay at 122,161 / year (average)
+DATE_FORMAT = '%I:%M %p - %a %b-%d-%Y'
+
+## Util functions
+
+def getAirportCodeQuery(apCode):
+    return '\n'.join([
+        "SELECT lat, lon from flights.airports"
+        " WHERE APCode='" + apCode + "'"
+        " LIMIT 1"
+    ])
+
+def getWaypointForFlightIdQuery(flightId):
+    return """
+        select *
+        from waypoints
+        where FlightId = 
+        """ + str(flightId)
 
 conn = cymysql.connect(
     host='127.0.0.1', 
@@ -18,26 +41,21 @@ cur.execute(
     """
     )
 
+def formatDate(dateString):
+    return datetime.strptime(
+        dateString.replace(' (runway)', ''),
+        DATE_FORMAT
+        )
+
 flights = [flight for flight in cur.fetchall()]
-
-def getAirportCodeQuery(apCode):
-    return '\n'.join([
-        "SELECT lat, lon from flights.airports"
-        " WHERE APCode='" + apCode + "'"
-        " LIMIT 1"
-    ])
-
-def getWaypointForFlightIdQuery(flightId):
-    return """
-        select *
-        from waypoints
-        where FlightId = 
-        """ + str(flightId)
 
 for flight in flights:
     flightId, dept, dest, time_dept, time_dest = flight
     
-    print(flight)
+    time_dept = formatDate(time_dept)
+    time_dest = formatDate(time_dest)
+
+    print((time_dept, time_dest))
 
     cur.execute(getAirportCodeQuery(dept))
 
