2673f0bc666f482a571d965f1d59bb770602b437
Benjamin Kovach] <bkovach5@uga.edu>
2014-12-01 20:26:57 -0500

    Waypoint stuff

--- src/TermProject/flight_calculator.py
+++ src/TermProject/flight_calculator.py
@@ -9,7 +9,48 @@ conn = cymysql.connect(
     )
 
 cur = conn.cursor()
-cur.execute('select * from flights')
 
-for r in cur.fetchall():
-    print(r);
+cur.execute(
+    """
+    select FlightId, dept, dest, time_dept, time_dest
+    from flights
+    limit 100
+    """
+    )
+
+flights = [flight for flight in cur.fetchall()]
+
+def getAirportCodeQuery(apCode):
+    return '\n'.join([
+        "SELECT lat, lon from flights.airports"
+        " WHERE APCode='" + apCode + "'"
+        " LIMIT 1"
+    ])
+
+for flight in flights:
+    flightId, dept, dest, time_dept, time_dest = flight
+    
+    print(flight)
+
+    cur.execute(getAirportCodeQuery(dept))
+
+    deptAirport = [ap for ap in cur.fetchall()][0]
+
+    cur.execute(getAirportCodeQuery(dest))
+
+    destAirport = [ap for ap in cur.fetchall()][0]
+
+    print(deptAirport)
+    print(destAirport)
+
+    cur.execute(
+        """
+        select *
+        from waypoints
+        where FlightId = 
+        """ + str(flightId)
+    )
+
+    waypoints = [waypoint for waypoint in cur.fetchall()]
+
+    print(len(waypoints))
